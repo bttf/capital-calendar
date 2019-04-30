74759099813be09c136d2f606029e969866cfe70
Adnan Chowdhury <adnanchowdhury88@gmail.com>
2019-04-29 18:27:24 -0700

    Add initial schema for transaction monitor

---
+++ api/src/schema/transactionMonitor/index.js
@@ -0,0 +1,22 @@
+import { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLEnumType } from 'graphql';
+
+const TransactionMonitorCadenceEnumType = new GraphQLEnumType({
+  name: 'TransactionMonitorCadenceEnum',
+  values: {
+    DAILY: { value: 'daily' },
+    WEEKLY: { value: 'weekly' },
+    MONTHLY: { value: 'monthly' },
+  },
+});
+
+export default new GraphQLObjectType({
+  name: 'TransactionMonitor',
+  fields: {
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    cadence: {
+      type: new GraphQLNonNull(TransactionMonitorCadenceEnumType),
+    },
+  },
+});
