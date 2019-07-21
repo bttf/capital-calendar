9f21f5c3afaa18a60a31f9a38f603bbfc3715977
Adnan Chowdhury <adnanchowdhury88@gmail.com>
2019-07-21 15:34:28 -0700

    Set up deployment

--- plaid-bookkeeper/src/index.js
+++ plaid-bookkeeper/src/index.js
@@ -1,7 +1,7 @@
 import Queue from 'bull';
 
-const { REDIS_HOST } = process.env;
-const itemQueue = new Queue('plaid-item', { redis: { host: REDIS_HOST } });
+const { REDIS_HOST, REDIS_PASSWORD } = process.env;
+const itemQueue = new Queue('plaid-item', { redis: { host: REDIS_HOST, password: REDIS_PASSWORD } });
 
 itemQueue.process((job, done) => {
   // eslint-disable-next-line no-console
--- plaid-webhook-handler/src/index.js
+++ plaid-webhook-handler/src/index.js
@@ -3,9 +3,9 @@ import Queue from 'bull';
 import bodyParser from 'body-parser';
 
 const PORT = process.env.PORT || 3002;
-const { REDIS_HOST } = process.env;
+const { REDIS_HOST, REDIS_PASSWORD } = process.env;
 const app = express();
-const itemQueue = new Queue('plaid-item', { redis: { host: REDIS_HOST } });
+const itemQueue = new Queue('plaid-item', { redis: { host: REDIS_HOST, password: REDIS_PASSWORD } });
 
 // TODO possibly remove if not utilized
 app.use(bodyParser.urlencoded({ extended: false }));
