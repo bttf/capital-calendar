a11caa32b1ef8a91e101e44c6ea2e12569eacc42
Adnan Chowdhury <adnanchowdhury88@gmail.com>
2019-08-12 21:42:11 -0700

    Oops

--- api/src/db/models/plaidTransaction.js
+++ api/src/db/models/plaidTransaction.js
@@ -1,46 +1,53 @@
 export default (sequelize, DataTypes) => {
-  const PlaidTransaction = sequelize.define('PlaidTransaction', {
-    transactionId: {
-      type: DataTypes.TEXT,
-      field: 'transaction_id',
-      allowNull: false,
+  const PlaidTransaction = sequelize.define(
+    'PlaidTransaction',
+    {
+      transactionId: {
+        type: DataTypes.TEXT,
+        field: 'transaction_id',
+        allowNull: false,
+      },
+      transactionType: {
+        type: DataTypes.TEXT,
+        field: 'transaction_type',
+        allowNull: false,
+      },
+      date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+      category: {
+        type: DataTypes.ARRAY(DataTypes.TEXT),
+      },
+      categoryId: {
+        type: DataTypes.TEXT,
+        field: 'category_id',
+      },
+      name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      amount: {
+        type: DataTypes.DECIMAL(13, 4),
+        allowNull: false,
+      },
+      pending: {
+        type: DataTypes.BOOLEAN,
+      },
+      pendingTransactionId: {
+        type: DataTypes.TEXT,
+        field: 'pending_transaction_id',
+      },
+      accountId: {
+        type: DataTypes.TEXT,
+        field: 'account_id',
+      },
     },
-    transactionType: {
-      type: DataTypes.TEXT,
-      field: 'transaction_type',
-      allowNull: false,
+    {
+      schema: 'app',
+      tablename: 'plaid_transactions',
     },
-    date: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-    },
-    category: {
-      type: DataTypes.ARRAY(DataTypes.TEXT),
-    },
-    categoryId: {
-      type: DataTypes.TEXT,
-      field: 'category_id',
-    },
-    name: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    amount: {
-      type: DataTypes.DECIMAL(13, 4),
-      allowNull: false,
-    },
-    pending: {
-      type: DataTypes.BOOLEAN,
-    },
-    pendingTransactionId: {
-      type: DataTypes.TEXT,
-      field: 'pending_transaction_id',
-    },
-    accountId: {
-      type: DataTypes.TEXT,
-      field: 'account_id',
-    },
-  });
+  );
 
   PlaidTransaction.associate = models => {
     const { PlaidTransaction, PlaidAccount } = models;
