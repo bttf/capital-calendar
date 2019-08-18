b2cf029029e03ce70fe70041a92a9f7fb8c99606
Adnan Chowdhury <adnanchowdhury88@gmail.com>
2019-08-17 17:22:15 -0700

    Add migration

---
+++ api/migrations/20190817234044-add-calendar-events-table.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* eslint-disable */
+var dbm;
+var type;
+var seed;
+/* eslint-enable */
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  const sql = `
+    CREATE TABLE app.calendar_events (
+      id SERIAL PRIMARY KEY,
+      event_id uuid NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+      date DATE NOT NULL,
+      summary TEXT,
+      color_id TEXT,
+      google_calendar_id TEXT NOT NULL,
+      calendar_id INTEGER NOT NULL REFERENCES app.calendars ON DELETE CASCADE,
+    );
+  `;
+  return db.runSql(sql);
+};
+
+exports.down = function(db) {
+  const sql = `
+    DROP TABLE app.calendar_events;
+  `;
+  return db.runSql(sql);
+};
+
+exports._meta = {
+  version: 1,
+};
